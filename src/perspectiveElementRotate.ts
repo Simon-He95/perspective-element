fe8570fb080c9cf42c7afbb862056674a8794997
Simon he <13917107460@163.com>
2022-09-17 18:41:16 +0800

    feat: add perspectiveElementRotate

---
+++ src/perspectiveElementRotate.ts
@@ -0,0 +1,28 @@
+import { addEventListener } from 'simon-js-tool'
+export function perspectiveElementRotate(container: HTMLImageElement | string, frequency = 0.5): () => void {
+  const stopMove = addEventListener(container, 'mousemove', (e) => {
+    const el = e.target as HTMLElement
+    const left = el.getBoundingClientRect().left
+    const top = el.getBoundingClientRect().top
+    const w = el.getBoundingClientRect().width
+    const h = el.getBoundingClientRect().height
+    const scrollTop = document.documentElement.scrollTop + document.body.scrollTop
+    const mouseX = e.pageX - left
+    const mouseY = e.pageY - scrollTop - top
+
+    const X = (mouseX - w / 2) / w / 100
+    const Y = (h / 2 - mouseY) / h / 100
+    el.style.transform = `perspective(300px) rotateY(${X * frequency}deg) rotateX(${Y * frequency}deg)`
+    el.style.boxShadow = `${-X / 20}px ${Y / 20}px 50px rgba(0, 0, 0, 0.3)`
+  })
+
+  const stopLeave = addEventListener(container, 'mouseleave', (e) => {
+    const el = e.target as HTMLElement
+    el.style.transform = 'perspective(300px) rotateY(0deg) rotateX(0deg)'
+    el.style.boxShadow = ''
+  })
+  return () => {
+    stopMove()
+    stopLeave()
+  }
+}
